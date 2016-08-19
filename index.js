a16180d0c67f5233f234c5b44225c53b9577acc9
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-08-18 20:42:08 -0400

    Add starter code

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var isValidSudoku = function(board){
+  // Your Code Here
+};
+
+var isSolved = function(board){
+  // Your Code Here
+};
+
+//// Reach Targets ////
+var isValidSudokuMove = function(board, row, col, val){
+  // Your Code Here
+};
+
+module.exports = {
+  isValidSudoku: isValidSudoku,
+  isSolved: isSolved,
+  isValidSudokuMove: isValidSudokuMove
+}
